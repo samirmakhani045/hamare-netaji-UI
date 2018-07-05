471114d215af4cc91768c7c2f30a832ff7e8cfc6
samirmakhani045 <samirmakhani045@gmail.com>
2018-07-05 19:37:39 +0530

    mmodel window set

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+ 
 
  
 
